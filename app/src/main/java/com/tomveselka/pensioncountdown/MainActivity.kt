0620ba7697fc0f4d4e936716175eb98440754269
vesely.tomas1234@seznam.cz <vesely.tomas1234@seznam.cz>
2021-11-06 23:32:26 +0100

    Cleaned up MainActivity

--- app/src/main/java/com/tomveselka/pensioncountdown/MainActivity.kt
+++ app/src/main/java/com/tomveselka/pensioncountdown/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.tomveselka.pensioncountdown
 
-import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.time.Duration
-import java.time.LocalDate
-import java.time.LocalTime
-import java.time.ZoneId
-import java.time.temporal.ChronoUnit
-import kotlin.math.ceil
-import com.tomveselka.pensioncountdown.R.id.*
 import com.tomveselka.pensioncountdown.utils.DateAndTimeCalculator
 
 
@@ -42,53 +34,11 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setTimeRemaining (){
-        /*
-        var diff = Duration.between(LocalTime.now(), LocalTime.MAX)
-
-        var hours = diff.toHours()
-        val hoursString = resources.getQuantityString(R.plurals.number_of_hours, hours.toInt(), hours)
-        diff = diff.minusHours(hours);
-
-        var minutes = diff.toMinutes()
-        val minutesString = resources.getQuantityString(R.plurals.number_of_minutes, minutes.toInt(), minutes)
-        diff = diff.minusMinutes(minutes);
-
-        var seconds = ceil((diff.toMillis()/1000).toDouble()).toInt()
-        val secondsString = resources.getQuantityString(R.plurals.number_of_seconds, seconds, seconds)
-
-         */
-        //timeView.setText(String.format(resources.getString(R.string.hours,hoursString, minutesString,secondsString)))
         timeView.setText(dateAndTimeCalculator.calculateTime(this))
-
     }
 
     private fun setDaysRemaining(){
-        /*
-        var birthDate = LocalDate.of(1969,11,28)
-        var pensionDate = birthDate.plusYears(65)
-
-        var yearsBetween = ChronoUnit.YEARS.between(LocalDate.now(), pensionDate)
-        val yearsString = resources.getQuantityString(R.plurals.number_of_years, yearsBetween.toInt(), yearsBetween)
-
-        var monthsBetween =  0L
-        if (LocalDate.now().month==birthDate.month){
-            monthsBetween = 0L
-        }else if (LocalDate.now().monthValue<birthDate.monthValue) {
-            monthsBetween = (birthDate.monthValue-LocalDate.now().monthValue).toLong()
-        }else{
-            monthsBetween = ChronoUnit.MONTHS.between(
-                LocalDate.now(),
-                birthDate.withYear(LocalDate.now().plusYears(1).year)
-            )
-        }
-        val monthsString = resources.getQuantityString(R.plurals.number_of_months, monthsBetween.toInt(), monthsBetween)
-
-        var daysBetween = (pensionDate.minusYears(yearsBetween).minusMonths(monthsBetween).toEpochDay() - LocalDate.now().toEpochDay())
-        val daysString = resources.getQuantityString(R.plurals.number_of_days, daysBetween.toInt(), daysBetween)
-        */
-        //https://stackoverflow.com/questions/42340687/two-or-more-plulars-in-one-string-in-android-xml
         dateView.setText(dateAndTimeCalculator.calculateDate(this))
-
     }
 
 }
